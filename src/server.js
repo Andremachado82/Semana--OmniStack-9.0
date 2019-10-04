1589601d3cbe8979658fab979944849c0eba0820
André Machado <andrels.machado82@gmail.com>
2019-10-04 18:49:53 -0300

    Configurando conexão com sockte-io

--- src/server.js
+++ src/server.js
@@ -3,14 +3,34 @@ const mongoose = require ('mongoose');
 const cors = require ('cors');
 const path = require ('path');
 
+const socketio = require ('socket.io');
+const http = require ('http');
+
 const routes = require ('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect('mongodb+srv://aircnc:aircnc@cluster0-yeqlo.mongodb.net/semana-9?retryWrites=true&w=majority',{
  useNewUrlParser : true,
  useUnifiedTopology:true,
-})
+});
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
 
 app.use(cors());
 app.use(express.json());
@@ -18,4 +38,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 
-app.listen (3001);
+server.listen (3333);
